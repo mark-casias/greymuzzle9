805e5b1d02d5f627709e2963d52dc57242f22d2c
Mark Casias <markie@kanopistudios.om>
2021-01-30 11:10:19 -0700

    Adding theme source

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -1,33 +1,20 @@
 <?php
 
 /**
- * Load services definition file.
+ * @file
+ * Settings.
  */
+
+// Load services definition file.
 $settings['container_yamls'][] = __DIR__ . '/services.yml';
 
-/**
- * Include the Pantheon-specific settings file.
- *
- * n.b. The settings.pantheon.php file makes some changes
- *      that affect all environments that this site
- *      exists in.  Always include this file, even in
- *      a local development environment, to ensure that
- *      the site settings remain consistent.
- */
+// Include the Pantheon-specific settings file.
 include __DIR__ . "/settings.pantheon.php";
 
-/**
- * Skipping permissions hardening will make scaffolding
- * work better, but will also raise a warning when you
- * install Drupal.
- *
- * https://www.drupal.org/project/drupal/issues/3091285
- */
-// $settings['skip_permissions_hardening'] = TRUE;
+// Hard coding config.
+$settings['config_sync_directory'] = '../config';
 
-/**
- * If there is a local settings file, then include it
- */
+// If there is a local settings file, then include it.
 $local_settings = __DIR__ . "/settings.local.php";
 if (file_exists($local_settings)) {
   include $local_settings;
---
+++ web/themes/custom/greymuzzle/tailwind.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+      white: '#fff',
+      black: '#000',
+      'gray-tundora': '#444',
+      'gray-gallery': '#ccc',
+      'gray-gallery-light': '#eee',
+      'blue-chill': '#067799',
+      'blue-botticelli': '#cfe5eb',
+      aliceblue: '#f3f8fa',
+      'burnt-sienna': '#f06667',
+      sunglow: '#ffcc33',
+      daffodil: '#fff5d6',
+      yellow: '#ffff00'
+    },
+    fontFamily: {
+      sans: ['Montserrat', 'Arial', 'sans-serif'],
+      serif: ['Montserrat', 'Arial', 'sans-serif'],
+      dyslexic: ['opendyslexic', 'sans-serif'],
+      fa: ['FontAwesome', 'sans-serif']
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
