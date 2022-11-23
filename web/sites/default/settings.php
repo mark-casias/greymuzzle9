57864dabcd560a422fc63eb7193c721ab3ada7b8
mohit <mohit.verma@kelltontech.com>
2022-11-23 16:13:18 +0530

    #comment settings

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -1,27 +1,34 @@
 <?php
 
 /**
- * @file
- * Settings.
+ * Load services definition file.
  */
-
-// Load services definition file.
 $settings['container_yamls'][] = __DIR__ . '/services.yml';
 
-// Include the Pantheon-specific settings file.
+/**
+ * Include the Pantheon-specific settings file.
+ *
+ * n.b. The settings.pantheon.php file makes some changes
+ *      that affect all environments that this site
+ *      exists in.  Always include this file, even in
+ *      a local development environment, to ensure that
+ *      the site settings remain consistent.
+ */
 include __DIR__ . "/settings.pantheon.php";
 
-// Hard coding config.
-$settings['config_sync_directory'] = '../config';
+/**
+ * Skipping permissions hardening will make scaffolding
+ * work better, but will also raise a warning when you
+ * install Drupal.
+ *
+ * https://www.drupal.org/project/drupal/issues/3091285
+ */
+// $settings['skip_permissions_hardening'] = TRUE;
 
-// If there is a local settings file, then include it.
+/**
+ * If there is a local settings file, then include it
+ */
 $local_settings = __DIR__ . "/settings.local.php";
 if (file_exists($local_settings)) {
   include $local_settings;
-}
-
-// Automatically generated include for settings managed by ddev.
-$ddev_settings = dirname(__FILE__) . '/settings.ddev.php';
-if (getenv('IS_DDEV_PROJECT') == 'true' && is_readable($ddev_settings)) {
-  require $ddev_settings;
 }
