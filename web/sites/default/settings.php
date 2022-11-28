16041f9eaae14e0814bb43beae87042ad4300c7d
mark-casias <mark.casias@kanopi.com>
2022-11-27 22:55:39 -0700

    Restoring settings and greymuzzle module

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -5,6 +5,9 @@
  */
 $settings['container_yamls'][] = __DIR__ . '/services.yml';
 
+// Hard coding config.
+$settings['config_sync_directory'] = '../config';
+
 /**
  * Include the Pantheon-specific settings file.
  *
@@ -32,3 +35,10 @@ $local_settings = __DIR__ . "/settings.local.php";
 if (file_exists($local_settings)) {
   include $local_settings;
 }
+
+// Automatically generated include for settings managed by ddev.
+$ddev_settings = dirname(__FILE__) . '/settings.ddev.php';
+if (getenv('IS_DDEV_PROJECT') == 'true' && is_readable($ddev_settings)) {
+  require $ddev_settings;
+}
+
