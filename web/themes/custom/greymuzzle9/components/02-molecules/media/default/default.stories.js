364f79c636c84cd7fe214d0d6bb91e7d9822cee1
mark-casias <mark.casias@kanopi.com>
2022-12-28 14:32:06 -0700

    media css and add2any updates

--- web/themes/custom/greymuzzle9/components/02-molecules/media/default/default.stories.js
+++ web/themes/custom/greymuzzle9/components/02-molecules/media/default/default.stories.js
@@ -22,5 +22,7 @@ export const Default = ({ photoCredit }) =>
   imageTwig({
     content: {
       field_photo_credit: [photoCredit],
+      field_media_image:
+        '<img height="213" width="320" style="width: 320px; height: 213px;" class="media-element file-basic-page-320x240-left" data-delta="1" typeof="foaf:Image" src="https://www.greymuzzle.org/sites/default/files/styles/basic_page_320x240/public/paterno.jpg?itok=aD7GCO2Y" alt="" />',
     },
   });
